5ac3171ff7e6ee70081ae4ad56c1566e35e2ae48
Prateek Gogia <gogia.prateek@hotmail.com>
2018-04-02 18:41:54 +0530

    Created linked-list.js

---
+++ linked-list.js
@@ -0,0 +1,60 @@
+class LinkedList {
+    constructor(value) {
+        this.head = null;
+        this.length = 0;
+        this.addToHead(value);
+    }
+    addToHead(value) {
+        const newNode = { value };
+        newNode.next = this.head;
+        this.head = newNode;
+        this.length++;
+        return this;
+    }
+    removeFromHead() {
+        if (this.length === 0) {
+            return undefined;
+        }
+        const value = this.head.value;
+        this.head = this.head.next;
+        this.length--;
+
+        return value;
+    }
+    find(val) {
+        let thisNode = this.head;
+
+        while (thisNode) {
+            if (thisNode.value === val) {
+                thisNode = thisNode.next;
+            }
+            return thisNode;
+        }
+    }
+    remove(val) {
+        if (this.length === 0) {
+            return undefined;
+        }
+        if (this.head.value === val) {
+            return this.removeFromHead();
+        }
+        let previousNode = this.head;
+        let thisNode = previousNode.next;
+
+        while (thisNode) {
+            if (thisNode.value === val) {
+                break;
+            }
+            previousNode = thisNode;
+            thisNode = thisNode.next;
+        }
+        if (thisNode === null) {
+            return undefined;
+        }
+
+        previousNode.next = thisNode.next;
+        this.length--;
+        return this;
+    }
+}
+console.log(JSON.stringify(new LinkedList('first').addToHead('second').addToHead('third').addToHead('fourth').remove('third')));
